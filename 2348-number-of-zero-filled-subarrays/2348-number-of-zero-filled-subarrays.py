a02abda4f6317b70b9569de1434734af04808cdf
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-08-14 20:38:43 +0530

    Time: 32 ms (90.24%), Space: 18.4 MB (36.59%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def zeroFilledSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        total = 0
+
+        for num in nums:
+            if num == 0:
+                count += 1
+                total += count 
+
+            else:
+                count = 0
+
+        return total 
+
+        
