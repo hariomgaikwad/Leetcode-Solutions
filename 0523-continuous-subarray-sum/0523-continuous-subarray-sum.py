e605cc461f751086af0a224eb18118fabf7602bd
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-09-25 23:19:28 +0530

    Time: 47 ms (91.52%), Space: 39.5 MB (79.09%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkSubarraySum(self, nums: list[int], k: int) -> bool:
+        
+        remainder_index = {0: -1}
+        prefix_sum = 0
+
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+
+            remainder = prefix_sum % k
+
+            if remainder in remainder_index:
+                if i - remainder_index[remainder] >= 2:
+                    return True
+            else:
+                remainder_index[remainder] = i
+
+        return False
+        
